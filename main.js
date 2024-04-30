baa0c0d1da0f7df1d5469fc9dfce24bc445ff4da
ParabolHoang <hoangvietquang939@gmail.com>
2024-04-30 08:13:51 +0700

    Done login and sing up page

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-<script src="https://unpkg.com/scrollreveal"></script>
-ScrollReveal().reveal(body);
+// Check the user input
+function compare() {
+    let user = document.getElementById("email").value;
+    let pass = document.getElementById("password").value;
+
+    console.log(user);
+    console.log(pass);
+}
+
+// Check the user have enter the pass or email
+function checkEmailAndPassword() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    if (!email) {
+      alert("You have not enter the email");
+    } else if (!password) {
+      alert("You have not enter the password");
+    }
+  }
